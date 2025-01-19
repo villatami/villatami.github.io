4998357eaaf06b3d4a25ae9d6f3c41c4e57f88d2
villatami <villatami@gmail.com>
2025-01-19 01:28:56 -0300

    innitial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+AOS.init({
+// Configuraciones que se pueden anular por elemento, mediante atributos `data-aos-*`:
+  offset: 120, // desplazamiento (en px) desde el punto de activación original
+  delay: 0, // valores de 0 a 3000, con pasos de 50ms
+  duration: 900, // valores de 0 a 3000, con pasos de 50ms
+  easing: 'ease', // suavización predeterminada para animaciones AOS
+  once: false, // si la animación debe ocurrir solo una vez, mientras se desplaza hacia abajo
+  mirror: false, // si los elementos deben animarse mientras se desplaza más allá de ellos
+  anchorPlacement: 'top-bottom', // define qué posición del elemento con respecto a la ventana debe activar la animación
+
+});
